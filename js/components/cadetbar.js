8e3e60082b097b96bfd7750a3b7491d7671434ec
Gymhgy <49801033+Gymhgy@users.noreply.github.com>
2022-11-29 21:49:27 +0000

    Deploying to gh-pages from @ Gymhgy/GGD@b7564c76ae7d1f7c841409a5c3e2736668b6a11e 🚀

--- js/components/cadetbar.js
+++ js/components/cadetbar.js
@@ -2,51 +2,56 @@
     const cadetBarTemplate = document.createElement('template');
   
     cadetBarTemplate.innerHTML = `
-    <div class="col-md-2 py-4 ps-4">
-        <a class="d-flex btn btn-light text-start w-100" href="pod.html">
-          <span class="material-symbols-outlined me-2">
-            description
-          </span>
-          <p>POD</p>
-        </a>
-        <a class="d-flex btn btn-light text-start w-100" href="calendar.html">
-          <span class="material-symbols-outlined me-2">
-            calendar_month
-          </span>
-          <p>Calendar</p>
-        </a>
-        <a class="d-flex btn btn-light text-start w-100" href="chainofcommand.html">
-          <span class="material-symbols-outlined me-2">
-            person
-          </span>
-          <p>Chain of Command</p>
-        </a>
-        <a class="d-flex btn btn-light text-start w-100" href="supply.html">
-          <span class="material-symbols-outlined me-2">
-            import_contacts
-          </span>
-          <p>Supply</p>
-        </a>
-        <a class="d-flex btn btn-light text-start w-100" href="https://quarterdeck.seacadets.org/">
-          <span class="material-symbols-outlined me-2">
-            military_tech
-          </span>
-          <p>Quarterdeck</p>
-        </a>
-        <a class="d-flex btn btn-light text-start w-100" href="payment.html">
-          <span class="material-symbols-outlined me-2">
-            attach_money
-          </span>
-          <p>Payment</p>
-        </a>
-        <a class="d-flex btn btn-light text-start w-100" href="">
-          <span class="material-symbols-outlined me-2">
-            directions_run
-          </span>
-          <p>PRT Standards</p>
-        </a>
-      </div>
-    </div>
+    <div>
+    <a class="d-flex btn btn-light text-start w-100" href="pod.html">
+      <span class="material-symbols-outlined me-2">
+        description
+      </span>
+      POD
+    </a>
+    <a class="d-flex btn btn-light text-start w-100" href="calendar.html">
+      <span class="material-symbols-outlined me-2">
+        calendar_month
+      </span>
+      Calendar
+    </a>
+    <a class="d-flex btn btn-light text-start w-100" href="chainofcommand.html">
+      <span class="material-symbols-outlined me-2">
+        person
+      </span>
+      Chain of Command
+    </a>
+    <a class="d-flex btn btn-light text-start w-100" href="supply.html">
+      <span class="material-symbols-outlined me-2">
+        checkroom
+      </span>
+      Supply
+    </a>
+    <a class="d-flex btn btn-light text-start w-100" href="resources.html">
+      <span class="material-symbols-outlined me-2">
+        import_contacts
+      </span>
+      Resources
+    </a>
+    <a class="d-flex btn btn-light text-start w-100" href="https://quarterdeck.seacadets.org/">
+      <span class="material-symbols-outlined me-2">
+        military_tech
+      </span>
+      Quarterdeck
+    </a>
+    <a class="d-flex btn btn-light text-start w-100" href="payment.html">
+      <span class="material-symbols-outlined me-2">
+        attach_money
+      </span>
+      Payment
+    </a>
+    <a class="d-flex btn btn-light text-start w-100" href="">
+      <span class="material-symbols-outlined me-2">
+        directions_run
+      </span>
+      PRT Standards
+    </a>
+</div>
     `;
     class CadetBar extends HTMLElement {
       constructor() {
