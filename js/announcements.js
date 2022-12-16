b7d3df173d48137d9190939526bdf108f4763dc1
WabblerthisGUY <79024292+WabblerthisGUY@users.noreply.github.com>
2022-12-16 10:58:59 -0800

    Update announcements.js

--- js/announcements.js
+++ js/announcements.js
@@ -1,6 +1,13 @@
 (() => {
     const announcementsInfo = [
         {
+            title: "Drill Photos",
+            text: "Our sponsors at the Alameda Navy League are requesting a couple of photos for an article to post in their newsletter. If you have any photos of the past two drills that are newsletter worthy, please email me by 1700 tonight and I will choose a couple to send to them. Cadets who have gone to Photojournalism training, or who are signed up to go this winter, let your BATCOM know if you would like to be the unit phojo cadet and take photos at drills and events. The deadline to apply for this position is 07 JAN 23.",
+            datetime: "Fri 16 DEC 22",
+            announcer: "LT NORTON",
+            picture: "img/staff/kellynorton.png"
+        },
+        {
             title: "2023 Calendar",
             text: "The Calendar for 2023 is up!  Be sure to check it out.",
             datetime: "Sat 10 DEC 22",
