15a43f42ebb451c9d8007ff7d8140417c058e7de
WabblerthisGUY <79024292+WabblerthisGUY@users.noreply.github.com>
2022-12-21 02:03:17 +0000

    Deploying to gh-pages from @ Gymhgy/GGD@9b55d3cb11965003da80e165f0af5c68c7cdc5ed 🚀

--- js/announcements.js
+++ js/announcements.js
@@ -1,6 +1,13 @@
 (() => {
     const announcementsInfo = [
         {
+            title: "Service Jackets",
+            text: "If you did not get your service jacket at the potluck on Sunday last drill, that means there was something missing from the info we must have in there, or you had not yet been approved for the training. If you are going to Camp SLO, I will bring your record with me.  If you are going to Hilltop, I will give your record to ENS Khan. Everyone else, we must pick a time for you to pick up your record before you leave.  If you are going to a training at a location with another cadet and you want them to pick up your record, let me know. Please TEXT me if you are going to a training besides SLO or Hilltop, and we will figure out a pickup time and place. 925-579-4755 ",
+            datetime: "Mon 19 DEC 22",
+            announcer: "LT NORTON",
+            picture: "img/staff/kellynorton.png"
+        },
+        {
             title: "Drill Photos",
             text: "Our sponsors at the Alameda Navy League are requesting a couple of photos for an article to post in their newsletter. If you have any photos of the past two drills that are newsletter worthy, please email me by 1700 tonight and I will choose a couple to send to them. Cadets who have gone to Photojournalism training, or who are signed up to go this winter, let your BATCOM know if you would like to be the unit phojo cadet and take photos at drills and events. The deadline to apply for this position is 07 JAN 23.",
             datetime: "Fri 16 DEC 22",
