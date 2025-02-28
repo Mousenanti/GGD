e9b0f99d053ef1604337aebe7035cbb9a640c3b9
ethanctrooer <ethanctrooper@gmail.com>
2025-02-27 19:29:55 -0800

    Readded protected.js protections

--- js/protected.js
+++ js/protected.js
@@ -1,6 +1,6 @@
-if(document.cookie.indexOf("authorized") === 'replace me!') {
+//if(document.cookie.indexOf("authorized") === 'replace me!') {
 //use above for testing previews
-//if(document.cookie.indexOf("authorized") === -1) {
+if(document.cookie.indexOf("authorized") === -1) {
     console.log(1);
 
     window.location.replace(document.baseURI + "protected.html?redirect=" + encodeURIComponent(location.href.replace(document.baseURI, '')));
