3cc91255f69aae1552666e3111c124e3a5f6953a
mousenanti <ninalolk@gmail.com>
2025-03-12 15:05:43 -0700

    Spring Bingo

--- js/announcements.js
+++ js/announcements.js
@@ -9,15 +9,11 @@
             Uniform of the day: Type 3
             What to bring: hydration
             
-            We will be assisting with two volunteer events the evening of the 28th. The Monster Bash will be on the USS Hornet,
-            until 2200 at the latest, and the Boo Bingo will be at the Alameda American Legion Post, until 2100 at the latest.
-
-            Details to come.
-            
-            RSVP to CO Norton knorton@seacadets.org`,
-            datetime: "Sun 08 Oct 23",
+            We will be assisting with the Spring Bingo event. 
+            RSVP: <a href="https://forms.gle/SjDZpZ47wjipRW2p6">https://forms.gle/CCJZbiocpU6cXHpj6</a>
+            datetime: "Sat 22 Mar 25",
             announcer: "General Announcement",
-            picture: "img/staff/general.png"
+            picture: "img/staff/general.png"`
         },
         {
             title: "4th of July Parade and Cadet Tan's Chief Pinning",
@@ -191,48 +187,6 @@
             datetime: "Sat 11 FEB 23",
             announcer: "General Announcement",
             picture: "img/staff/general.png"
-        },
-        {
-            title: "Academic Report Card",
-            text: "If you would like to submit your academic report card for recognition in March, please use this link: https://forms.gle/gdK4e4K8LU7LsmQb8. Current period is for Fall semester 2022. Please submit before February 28, 2023.",
-            datetime: "Tue 31 JAN 23",
-            announcer: "INST MUI",
-            picture: "img/staff/instmui.png"
-        },
-        {
-            title: "New BATCOM",
-            text: "CPO Xander Mui is the new BATCOM. Please address all concerns appropriately through the chain of command. His phone number is 415-309-9494.",
-            datetime: "Sat 14 JAN 23",
-            announcer: "General Announcement",
-            picture: "img/staff/general.png"
-        },
-        {
-            title: "Service Jackets",
-            text: "If you did not get your service jacket at the potluck on Sunday last drill, that means there was something missing from the info we must have in there, or you had not yet been approved for the training. If you are going to Camp SLO, I will bring your record with me.  If you are going to Hilltop, I will give your record to ENS Khan. Everyone else, we must pick a time for you to pick up your record before you leave.  If you are going to a training at a location with another cadet and you want them to pick up your record, let me know. Please TEXT me if you are going to a training besides SLO or Hilltop, and we will figure out a pickup time and place. 925-579-4755 ",
-            datetime: "Mon 19 DEC 22",
-            announcer: "LT NORTON",
-            picture: "img/staff/kellynorton.jpg"
-        },
-        {
-            title: "Drill Photos",
-            text: "Our sponsors at the Alameda Navy League are requesting a couple of photos for an article to post in their newsletter. If you have any photos of the past two drills that are newsletter worthy, please email me by 1700 tonight and I will choose a couple to send to them. Cadets who have gone to Photojournalism training, or who are signed up to go this winter, let your BATCOM know if you would like to be the unit phojo cadet and take photos at drills and events. The deadline to apply for this position is 07 JAN 23.",
-            datetime: "Fri 16 DEC 22",
-            announcer: "LT NORTON",
-            picture: "img/staff/kellynorton.jpg"
-        },
-        {
-            title: "2023 Calendar",
-            text: "The Calendar for 2023 is up!  Be sure to check it out.",
-            datetime: "Sat 10 DEC 22",
-            announcer: "LT NORTON",
-            picture: "img/staff/kellynorton.jpg"
-        },
-        {
-            title: "PRT",
-            text: "If you are attending a training this winter, make sure you have a passing PRT score on file. We'll be conducting PRT next drill—start training by doing push ups, practicing planks, and running miles.",
-            datetime: "Wed 29 SEP 22",
-            announcer: "LT NORTON",
-            picture: "img/staff/kellynorton.jpg"
         }
     ]
 
